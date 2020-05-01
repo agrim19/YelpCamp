4dc9e53c5bf23cfee294be943da24108ddc0f504
Agrim <agrim19229@iiitd.ac.in>
2020-05-01 15:11:59 +0530

    add mongodb atlas

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ var campgroundRoutes=require("./routes/campgrounds");
     indexRoutes=require("./routes/index");
 
 // seedDB();        //seed the database
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://agrim:Superman@123@cluster0-kp0gc.mongodb.net/test?retryWrites=true&w=majority", {useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    console.log("connected to DB");
+}).catch(err=>{
+    console.log("ERROR ",err.message);
+});
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));           //tells express to serve from the public folder too
 app.use(bodyParser.urlencoded({extended:true}));
